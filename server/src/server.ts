dede8543004d070706b79ba660d65ea0c072ba47
JacobJEdwards <jacob.john1@talktalk.net>
2023-04-21 14:43:46 +0100

    improved port normalization function

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
 import http from "http";
 import express, { Express, Request, Response, NextFunction } from "express";
 import cors from "cors";
-import helmet, { xssFilter } from "helmet";
+import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
 
 import normalizePort from "./utils/normalizePort";
-import routes from "./routes";
+import APIRoutes from "./routes/api.router";
 
 dotenv.config();
 
@@ -25,23 +25,23 @@ app.use(morgan("dev"));
 app.disable("x-powered-by");
 
 // routes
-app.use("/api", routes);
+app.use("/api", APIRoutes);
 
 // error handling
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    return res.status(500).json({
-        message: err.message,
-    });
+  return res.status(500).json({
+    message: err.message,
+  });
 });
 
 // 404 error handling
 app.use((req: Request, res: Response, next: NextFunction) => {
-    const error = new Error("Not found");
-    return res.status(404).json({ message: error.message });
+  const error = new Error("Not found");
+  return res.status(404).json({ message: error.message });
 });
 
 // server initialization
-const port: number | string | boolean = normalizePort(process.env.PORT || 6060);
+const port = normalizePort(process.env.PORT || 6060) as number;
 if (!port) throw new Error("Port is not defined");
 
 app.set("port", port);
@@ -54,30 +54,30 @@ server.on("listening", onListening);
 
 // https://github.com/ferreiro/example-monorepo/blob/master/packages/server/bin/www#L15
 function onError(error: NodeJS.ErrnoException): void {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
+  if (error.syscall !== "listen") {
+    throw error;
+  }
 
-    const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
-    // handle specific liseners
-    switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.error(bind + " is already in use");
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  // handle specific liseners
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
 function onListening() {
-    const addr = server.address();
-    if (!addr) return;
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-    console.log("Listening on " + bind);
+  const addr = server.address();
+  if (!addr) return;
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log("Listening on " + bind);
 }
--- server/src/utils/normalizePort.ts
+++ server/src/utils/normalizePort.ts
@@ -1,21 +1,16 @@
-const normalizePort = (val: string | number): string | number | boolean => {
-    if (typeof val === "number") {
-        return val;
-    }
+const normalizePort = (val: unknown): string | number | false => {
+  if (typeof val === "number") {
+    return val;
+  }
 
-    const port = parseInt(val, 10);
+  const port = parseInt(val as string, 10);
 
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
+  if (!isNaN(port)) {
+    // named pipe
+    return port;
+  }
 
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-
-    return false;
+  return false;
 };
 
 export default normalizePort;
