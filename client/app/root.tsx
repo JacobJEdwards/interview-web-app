f8a2357fe23c71e70582fe0f7b48dc7683a25ac9
JacobJEdwards <jacob.john1@talktalk.net>
2023-04-21 16:01:15 +0100

    added error handlinga nd basic functionalit

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -1,60 +1,82 @@
 import {
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useRouteError,
+  isRouteErrorResponse,
 } from "@remix-run/react";
 
-import { PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
+
+import tailwindURL from "./styles/app.css";
 
 export const links: LinksFunction = () => {
-    return [{ rel: "stylesheet", href: "./styles/app.css" }];
+  return [{ rel: "stylesheet", href: tailwindURL }];
 };
 
-export const meta: MetaFunction = () => {
-    return {
-        description: "Interview Web App",
-    };
+export const meta: V2_MetaFunction = () => {
+  return [{ description: "Interview Web App" }];
 };
 
 const Layout = ({
-    children,
-    title = "Interview Web App",
+  children,
+  title = "Interview Web App",
 }: PropsWithChildren<{ title?: string }>) => (
-    <html lang="en">
-        <head>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width,initial-scale=1" />
-            <Meta />
-            <Links />
-            <title>{title}</title>
-        </head>
-        <body>
-            {children}
-            <ScrollRestoration />
-            <Scripts />
-            <LiveReload />
-        </body>
-    </html>
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <Meta />
+      <Links />
+      <title>{title}</title>
+    </head>
+    <body>
+      {children}
+      <ScrollRestoration />
+      <Scripts />
+      <LiveReload />
+    </body>
+  </html>
 );
 
 export default function App() {
-    return (
-        <Layout title="Home">
-            <Outlet />
-        </Layout>
-    );
+  return (
+    <Layout title="Home">
+      <Outlet />
+    </Layout>
+  );
 }
 
-export function ErrorBoundary({ error }: { error: Error }) {
+export function ErrorBoundary() {
+  const error = useRouteError();
+  if (isRouteErrorResponse(error)) {
     return (
-        <Layout title="Oops!">
-            <h1>Oops!</h1>
-            <pre>{error.message}</pre>
-        </Layout>
+      <Layout title="Oops!">
+        <main className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold text-gray-900">Oops!</h1>
+          <p className="text-gray-700">Something went wrong</p>
+          <pre className="text-sm text-gray-500">{error.message}</pre>
+        </main>
+      </Layout>
     );
+  }
+
+  let errorMessage = "An unknown error occurred";
+  if (error instanceof Error) {
+    errorMessage = error.message;
+  }
+
+  return (
+    <Layout title="Oops!">
+      <main className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold text-gray-900">Oops!</h1>
+        <p className="text-gray-700">{errorMessage}</p>
+      </main>
+    </Layout>
+  );
 }
