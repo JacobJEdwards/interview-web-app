6b52a91002cfef2253f8543399a9e93f328fa8d4
JacobJEdwards <jacob.john1@talktalk.net>
2023-05-04 14:52:18 +0100

    validating token with zod

--- packages/server/src/auth.ts
+++ packages/server/src/auth.ts
@@ -1,39 +1,50 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
+import schemas from "./schemas";
 
 dotenv.config();
 
 const validateToken = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
+  const token = req.headers.authorization;
 
-    if (!token) {
-        return res.status(401).json({
-            status: 401,
-            error: "Unauthorized",
-        });
+  if (!token) {
+    return res.status(401).json({
+      status: 401,
+      error: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY as string);
+    console.log(decoded);
+    console.log(typeof decoded);
+
+    if (!decoded) {
+      return res.status(401).json({
+        status: 401,
+        error: "Invalid token",
+      });
     }
 
-    try {
-        const decoded = jwt.verify(token, process.env.SECRET_KEY as string);
+    const validToken = schemas.token.parse(decoded);
 
-        if (!decoded) {
-            return res.status(401).json({
-                status: 401,
-                error: "Invalid token",
-            });
-        }
+    // req.user = decoded;
+    // will give the req object user info for use in future from token (? could be useful -> would have to extend Request interface and change type of routes)
+    // req.user = {
+    //   id: validToken.id,
+    //   email: validToken.email,
+    //   name: validToken.name,
+    //   role: validToken.role,
+    // };
 
-        // req.user = decoded;
-        // will give the req object user info for use in future from token (? could be useful -> would have to extend Request interface and change type of routes)
-
-        next();
-    } catch (error) {
-        return res.status(401).json({
-            status: 401,
-            error: "Invalid token",
-        });
-    }
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      status: 401,
+      error: "Invalid token",
+    });
+  }
 };
 
 export default validateToken;
--- packages/server/src/schemas.ts
+++ packages/server/src/schemas.ts
@@ -2,56 +2,67 @@ import { z } from "zod";
 
 // auth
 export const LoginSchema = z.object({
-  email: z.string().trim().email({
-    message: "Invalidation email",
-  }),
-  password: z.string(),
+    email: z.string().trim().email({
+        message: "Invalidation email",
+    }),
+    password: z.string(),
 });
 
 export const idSchema = z.coerce.number().int().positive();
 
 export const nameSchema = z.string().trim().min(1).max(255);
 export const descriptionSchema = z.string().trim().min(1).max(255);
+export const roleSchema = z.enum(["STUDENT", "TEACHER", "ADMIN"]);
 
 export const getModuleSchema = z.object({
-  name: nameSchema.optional(),
-  teacherId: idSchema.optional(),
+    name: nameSchema.optional(),
+    teacherId: idSchema.optional(),
+});
+
+export const tokenSchema = z.object({
+    name: nameSchema,
+    id: idSchema,
+    email: z.string().trim().email(),
+    role: roleSchema,
+    iat: z.number(),
+    exp: z.number(),
 });
 
 export const createModuleSchema = z.object({
-  name: nameSchema,
-  description: descriptionSchema,
-  teacherId: idSchema,
+    name: nameSchema,
+    description: descriptionSchema,
+    teacherId: idSchema,
 });
 
 export const updateModuleSchema = z.object({
-  name: nameSchema.optional(),
-  description: descriptionSchema.optional(),
-  teacherId: idSchema.optional(),
+    name: nameSchema.optional(),
+    description: descriptionSchema.optional(),
+    teacherId: idSchema.optional(),
 });
 
 export const createProjectSchema = z.object({
-  name: nameSchema,
-  description: descriptionSchema,
-  teacherId: idSchema,
+    name: nameSchema,
+    description: descriptionSchema,
+    teacherId: idSchema,
 });
 
 export const getProjectsSchema = z.object({
-  name: nameSchema.optional(),
-  teacherId: idSchema.optional(),
-  moduleId: idSchema.optional(),
+    name: nameSchema.optional(),
+    teacherId: idSchema.optional(),
+    moduleId: idSchema.optional(),
 });
 
 const schemas = {
-  login: LoginSchema,
-  id: idSchema,
-  name: nameSchema,
-  description: descriptionSchema,
-  getModule: getModuleSchema,
-  createModule: createModuleSchema,
-  updateModule: updateModuleSchema,
-  getProjects: getProjectsSchema,
-  createProject: createProjectSchema,
+    login: LoginSchema,
+    id: idSchema,
+    token: tokenSchema,
+    name: nameSchema,
+    description: descriptionSchema,
+    getModule: getModuleSchema,
+    createModule: createModuleSchema,
+    updateModule: updateModuleSchema,
+    getProjects: getProjectsSchema,
+    createProject: createProjectSchema,
 };
 
 export default schemas;
