5035917574d3dc67130e87704a36c9eba6b65ec7
JacobJEdwards <jacob.john1@talktalk.net>
2023-05-14 12:33:29 +0100

    added another index file

---
+++ packages/client/app/components/index.ts
@@ -0,0 +1,5 @@
+export { default as ProjectForm } from './ProjectForm';
+export { default as Project } from './Project';
+export { default as Module } from './Module';
+export {default as Footer} from './Footer';
+export { default as Breadcrumbs } from './Breadcrumbs';
